c8c9784c387720aa34e1edfbd7a731a02f818293
Imran Hashmi <imran.hoshimi@gmail.com>
2018-01-29 01:05:01 +0400

    Gui Design
    Funtionality
    - Splash
    - Login
    - Side Bar
    - Logout

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -7,40 +7,88 @@ public class StoreManager : MonoBehaviour {
 
 	public GameObject Splash;
 	public GameObject Login;
-	public GameObject StoreMenu;
+	public GameObject TopPanel;
+	public GameObject Sidebar;
+	public GameObject Home;
+	public GameObject Tasks;
+	public GameObject Team;
+	public GameObject Charts;
+	public GameObject Orders;
 
 	public InputField username;
 	public InputField password;
 
-	// Use this for initialization
-	void Start () {
-		Splash.SetActive (true);
+	void disableAllPanels ()
+	{
+		Splash.SetActive (false);
+		Login.SetActive (false);
+		TopPanel.SetActive (false);
+		Sidebar.SetActive (false);
+		Home.SetActive (false);
+		Tasks.SetActive (false);
+		Team.SetActive (false);
+		Charts.SetActive (false);
+		Orders.SetActive (false);
+
+		sidebarOpened = false;
 	}
 
-	IEnumerator OnOpenSplash()
-	{	
+	void Awake(){
+
+		disableAllPanels ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
 		
+		Splash.SetActive (true);
+
+		StartCoroutine( OpenSplash() );
+	} 
+
+	IEnumerator OpenSplash()
+	{	
+
+		yield return new WaitForSeconds (2f);
+
 		Splash.SetActive (false);
 		Login.SetActive (true);
 
 		yield break;
 	}
 
-	IEnumerator OnLoginSubmit()
+	public void OnLoginSubmit()
 	{	
 
 		if (username.text.ToLower () == "admin" && password.text.ToLower () == "admin") {
 
 			Login.SetActive (false);
-			StoreMenu.SetActive(true);
+			TopPanel.SetActive(true);
+
+			Home.SetActive (true);
 
 		} else {
 
 			Debug.Log ("Wrong Use\rname/Password !!!");
-		}
+		} 
+  	}
 
-		Splash.SetActive (false);
-		yield break;
+	public void OnLogout()
+	{	
+		disableAllPanels ();
+
+		username.text = "";
+		password.text = "";
+
+		Login.SetActive (true);
+ 	}
+
+	bool sidebarOpened = false;
+	public void ToggleSidebar()
+	{
+		sidebarOpened = !sidebarOpened;
+		Sidebar.SetActive (sidebarOpened);
 	}
 
 	// Update is called once per frame
